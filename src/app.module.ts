b0ef9c1607b7ed8517129bccfe5143ee6f6da2c0
vlad_vitkovskyi <vvitkovskij4@gmail.com>
2024-11-01 13:55:14 +0200

    chore: add some base structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './models/user/user.module';
+import { ConfigModule } from './config/config.module';
+import { DatabaseModule } from './database/database.module';
 
 @Module({
-  imports: [],
+  imports: [UserModule, ConfigModule, DatabaseModule],
   controllers: [AppController],
   providers: [AppService],
 })
