c68a21b391f2e439858ec3c693d69588551d403e
vlad_vitkovskyi <vvitkovskij4@gmail.com>
2025-03-27 21:49:49 +0200

    feat: upload user images

---
+++ src/models/aws/aws.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+
+import * as AWS from 'aws-sdk';
+
+@Injectable()
+export class AwsService {
+  private s3: AWS.S3;
+
+  constructor(private readonly configService: ConfigService) {
+    this.s3 = this.initS3();
+  }
+
+  private initS3() {
+    return new AWS.S3({
+      accessKeyId: this.configService.get<string>('s3.accessKeyId'),
+      secretAccessKey: this.configService.get<string>('s3.secretAccessKey'),
+      region: this.configService.get<string>('s3.region'),
+    });
+  }
+
+  async uploadFile(file: Express.Multer.File) {
+    const params = {
+      Bucket: this.configService.get<string>('s3.bucketName'),
+      Key: file.originalname,
+      Body: file.buffer,
+    };
+    return this.s3.upload(params).promise();
+  }
+}
